db0982231ae5f266b02aa0f7aa7b6ae5fd26b8e2
asalesalat <asalesalat90@gmail.com>
2024-09-16 10:07:49 -0700

    Create soal3.py

---
+++ tamrin1/soal3.py
@@ -0,0 +1,4 @@
+number = int(input("enter number:"))
+revere_num = int(str(number)[::-1])
+total = number + revere_num
+print(f"jam adad shoma {number} ba makous adad {revere_num} mosavi mibashad ba {total} ")
